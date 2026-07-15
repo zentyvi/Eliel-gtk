f8bfa7b1ccb3c6643841932bed687e9df310cecf
The Sco <goodmorninvo3@gmail.com>
2026-07-15 15:24:13 +0300

    Change colors of assets

--- scripts/build-gtk-4.0.sh
+++ scripts/build-gtk-4.0.sh
@@ -3,7 +3,7 @@
 sassc ./gtk-4.0/gtk.scss ./gtk-4.0/gtk.css
 sassc ./gtk-4.0/gtk.scss ./gtk-4.0/gtk-dark.css
 rm -rf ~/.config/gtk-4.0
-cp -r ./gtk-4.0 ~/.config/
+ln -sf ~/Repositories/Andromeda-gtk/gtk-4.0 ~/.config/gtk-4.0
 rm -rf ~/.cache/gtk-4.0
 
 nautilus -q
--- scripts/build-gtk.sh
+++ scripts/build-gtk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sh ./build-gtk-3.0.sh
-sh ./build-gtk-4.0.sh
+sh ./scripts/build-gtk-3.0.sh
+sh ./scripts/build-gtk-4.0.sh
