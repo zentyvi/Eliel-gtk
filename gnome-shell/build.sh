cee520ad47ff7c8af77f930a48845fd88a141145
The Sco <goodmorninvo3@gmail.com>
2026-07-15 23:09:44 +0300

    Fix gtk-3.0 dialog border

--- gnome-shell/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sassc ./gnome-shell.scss ./gnome-shell.css
-rm -rf ~/.themes/Andromeda-teal/gnome-shell
-cp -r ../gnome-shell ~/.themes/Andromeda-teal
-dconf write /org/gnome/shell/extensions/user-theme/name "'Default'"
-dconf write /org/gnome/shell/extensions/user-theme/name "'Andromeda-teal'"
--- gtk-4.0/build.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-sassc gtk.scss gtk.css
-sassc gtk.scss gtk-dark.css
-
-rm -rf ~/.config/gtk-4.0
-rm -rf ~/.cache/gtk-4.0
-cp -r ../gtk-4.0 ~/.config
